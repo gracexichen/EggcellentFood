1cb02847d39b8320abf98e584d1979f452ba5b2e
tiramisuuuuuuu <ssmathew@ucdavis.edu>
2023-11-04 18:11:23 -0700

    Create index.js

---
+++ eggcellent-app/server/index.js
@@ -0,0 +1,27 @@
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const fs = require('fs');
+
+const credentials = '<path_to_certificate>'
+
+const client = new MongoClient('mongodb+srv://eggcellentfood.4pvgzkk.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority', {
+  sslKey: credentials,
+  sslCert: credentials,
+  serverApi: ServerApiVersion.v1
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("testDB");
+    const collection = database.collection("testCol");
+    const docCount = await collection.countDocuments({});
+    console.log(docCount);
+    // perform actions using client
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
